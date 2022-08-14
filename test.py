ce695d2f20a87635fb8a4394146dc237ddf48ee5
unknown <anisadianislami105@gmail.com>
2022-08-14 10:42:50 +0700

    BASICPYTHON18AI

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
-x = [1,2,3,4,5]
-y = [2,4,3,5,6]
-z = 0
+#test1
+# #x = [1,2,3,4,5]
+#y = [2,4,3,5,6]
+#z = 0
 
-for i in x:
-    for j in y:
-        if i ==j:
-            z =z+1
+#for i in x:
+#   for j in y:
+#        if i ==j:
+#            z =z+1
+#print(z)
 
-print(z)
+#test2
+#a = "Hallo"
+#b = "World"
+#c = a + b
+#d = a + " " + b
+#print(d)
+
+age = 36
+txt = "My name is John, and I am {}".format(age)
+print(txt)
